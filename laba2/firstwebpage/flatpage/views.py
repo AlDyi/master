11b89f4d9c6a8490b80b4d739df0fdb59febd883
AlDyi <48719055+AlDyi@users.noreply.github.com>
2019-05-26 15:34:09 +0300

    Update views.py

--- laba2/firstwebpage/flatpage/views.py
+++ laba2/firstwebpage/flatpage/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
 
 def home(request):
-    return HttpResponse(u'Привет, Мир!')
+    return render(request, 'templates/static_handler.html')
