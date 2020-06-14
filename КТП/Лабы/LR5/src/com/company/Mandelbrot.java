5b1c3e2efb2002aa314ca2cffea5e9cef3e12728
AlDyi <48719055+AlDyi@users.noreply.github.com>
2020-06-14 20:52:20 +0300

    Update Mandelbrot.java

--- КТП/Лабы/LR5/src/com/company/Mandelbrot.java
+++ КТП/Лабы/LR5/src/com/company/Mandelbrot.java
@@ -28,9 +28,7 @@ public class Mandelbrot extends FractalGenerator
             x1 = x2;
             y1 = y2;
             if (x1 * x1 + y1 * y1 > 4)
-            {
                 return i;
-            }
         }
         return -1; // Going abroad
     }
